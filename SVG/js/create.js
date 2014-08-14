198d7cc6097d96f356e570966cd6584ea7819fb0
jamesjacko <james.ogge@gmail.com>
2014-08-14 13:59:30 +0100

    first commit to james branch

--- SVG/js/create.js
+++ SVG/js/create.js
@@ -1,123 +1,54 @@
 var environment = {};
-environment['shapes'] = {};
-window.onload = function () {
-             // Creates canvas 1000 × 1000 at 30, 100
-var paper = Raphael(30, 100, 1000, 1000);
+environment.shapes = Array();
+var shapeStruct = function(shape) {
+    this.r = shape.r || null;
+    this.x = shape.x;
+    this.y = shape.y;
+    this.type = shape.type;
+    this.id = shape.id || null;
+}
 
-var rect1 = paper.rect(0, 0, 1000, 1000);
-rect1.attr({fill: "#444"});
+window.onload = function() {
+    // Creates canvas 1000 × 1000 at 30, 100
+    var paper = Raphael(30, 100, 1000, 1000),
+        //rect1 = paper.rect(0, 0, 1000, 1000).attr({fill: "#444"}),
+        rect = paper.rect(55, 130, 50, 50).attr({fill: "#34C8FF",stroke: "0"});
+        circle = paper.circle(80, 100, 25).attr({fill: "#34C8FF",stroke: "0"});
+        path = paper.path("M50,68 L77,23 L105,68 z").attr({fill: "#34C8FF",stroke: "0"});
 
-//Create shapes
-var rect = paper.rect(55, 130, 50, 50);
-rect.attr({fill: "#34C8FF",stroke:"0" });
+        console.log(rect, circle, path)
+    var start = function(){
+            this.ox = this.attrs.x;
+            this.oy = this.attrs.y;
+            this.lastdx ? this.ox += this.lastdx : this.ox = 0;
+            this.lastdy ? this.oy += this.lastdy : this.oy = 0;
+            if(!this.clonedShape){
+                var cloned = this.clone();
+                cloned.drag(move, start, up);
+                this.cloneedShape = true;
+            }
+            
+    },
+        move = function(dx, dy){
+            this.transform("T" + (dx + this.ox) + "," + (dy + this.oy));
+            this.lastdx = dx;
+            this.lastdy = dy;
 
-var circle = paper.circle(80, 100, 25);
-circle.attr({fill: "#34C8FF",stroke:"0"});
+            environment.shapes[this.id] = this;
+    },
+        up = function(){
+            var ft = paper.freeTransform(this, {
+                keepRatio: true 
+            }, function(e){
+                environment.shapes[this.id] = this;
+            }).showHandles().apply(); 
+            console.log(environment.shapes)
+    };
+    paper.set(rect,circle,path)
+    rect.drag(move, start, up);
+    circle.drag(move, start, up);
+    path.drag(move, start, up);
 
-var path = paper.path("M50,68 L77,23 L105,68 z");
-path.attr({fill: "#34C8FF",stroke:"0"});
-
-
-
-
-circle.click(function (e) //Onclick create object
-{
-		var cloned = circle.clone(); 
-	  var length = environment['shapes'].length > 0 ? environment['shapes'].length : 0; 
-    	newCircle ={
-    	r: cloned.attr("r"),
-    	x:cloned.attr("cx"),
-    	y:cloned.attr("cy"),
-    	type:"circle",
-    	id: "circle" + length
-
-    	
-    }
-	
-						
-	var ft = paper.freeTransform(cloned);//Add handles to clone
-   
-  	ft.showHandles();
-	ft.apply();
-	ft = paper.freeTransform(cloned, { keepRatio: true	}, function(ft, events) {console.log(ft, event)});
-    ft.setOpts({ keepRatio: false }, function(e){environment['shapes'][e.id] = e});
-    
-    
-    
-	environment["shapes"][Object.keys(environment.shapes).length] = newCircle;
-		
-});
-
-
-
-rect.click(function (e) //Onclick create object
-{
-var	newRectangle ={
-    	
-    	x:rect.attr("x"),
-    	y:rect.attr("y"),
-    	height: rect.attr("height"),
-    	width: rect.attr("width"),
-    	type:"rectangle"
-
-    	
-    }
-   
-	var cloned = rect.clone();					
-	var ft = paper.freeTransform(cloned);//Add handles to clone
-    ft.showHandles();
-	ft.apply();
-	    ft = paper.freeTransform(cloned, { keepRatio: true }, function(ft, events) {
-
-    });
-    ft.setOpts({ keepRatio: false });
-					environment["shapes"][Object.keys(environment.shapes).length] = newRectangle;
-			
-});
-
-
-
-path.click(function (e) //Onclick create object
-{
-var	newPath ={
-    	
-    	x:0,
-    	y:0,
-    	height: 0,
-    	width: 0,
-    	type:"path"
-
-    	
-    }
-   
-	var cloned = path.clone();					
-	var ft = paper.freeTransform(cloned);//Add handles to clone
-    ft.showHandles();
-	ft.apply();
-	    ft = paper.freeTransform(cloned, { keepRatio: true }, function(ft, events) {
-
-    });
-    ft.setOpts({ keepRatio: false });
-					environment["shapes"][Object.keys(environment.shapes).length] = newPath;
-			
-});
-
-circle.drag(function (e) //Onclick create object
-{
-
-
-		newCircle ={
-    	r: cloned.attr("r"),
-    	x:cloned.attr("cx"),
-    	y:cloned.attr("cy"),
-    	type:"circle"
-
-    	
-    }
-
-});
 
 
 };
-
- 
