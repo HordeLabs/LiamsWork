f1df0cac825317753e0e0aebd616182e941f7903
jamesjacko <james.ogge@gmail.com>
2014-09-02 13:22:15 +0100

    adding objects to Environment

--- SVG/js/create.js
+++ SVG/js/create.js
@@ -1,12 +1,12 @@
 var environment = {};
 environment.shapes = Array();
-
+var paper = null;
 window.onload = function() {
     // Creates canvas 1000 × 1000 at 30, 100
-    var paper = Raphael(30, 100, 1000, 1000),
-        //rect1 = paper.rect(0, 0, 1000, 1000).attr({fill: "#444"}),
-        rect = paper.rect(55, 130, 50, 50).attr({fill: "#34C8FF",stroke: "0"});
-        circle = paper.circle(80, 100, 25).attr({fill: "#34C8FF",stroke: "0"});
+    // var rect1 = paper.rect(0, 0, 1000, 1000).attr({fill: "#444"}),
+        paper = Raphael(30, 100, 1000, 1000);
+    var rect = paper.rect(55, 130, 50, 50).attr({fill: "#34C8FF",stroke: "0"}),
+        circle = paper.circle(80, 100, 25).attr({fill: "#34C8FF",stroke: "0"}),
         path = paper.path("M50,68 L77,23 L105,68 z").attr({fill: "#34C8FF",stroke: "0"});
 
     var start = function(){ // function for the start of the drag
@@ -14,6 +14,9 @@ window.onload = function() {
             this.oy = this.attrs.y;
             this.lastdx ? this.ox += this.lastdx : this.ox = 0;
             this.lastdy ? this.oy += this.lastdy : this.oy = 0;
+            // console.log(this);
+            this.attrs.class = "added";
+            //this.setAttribute("class", "added");
             if(!this.clonedShape){ // if this shape is a clone then we don't want to create a new one
                 var cloned = this.clone();
                 cloned.drag(move, start, up);
@@ -26,18 +29,36 @@ window.onload = function() {
             this.lastdx = dx;
             this.lastdy = dy;
 
-            environment.shapes[this.id] = this;
+            // environment.shapes[this.id] = this;
     },
         up = function(){ // function for when the dragends
             var ft = paper.freeTransform(this, {
                 keepRatio: true // not working?
             }, function(e){
+                // console.log(e);
                 environment.shapes[this.id] = this;
             }).showHandles().apply(); 
-            console.log(environment.shapes)
+            // console.log(environment.shapes)
     };
     paper.set(rect,circle,path)
     rect.drag(move, start, up);
     circle.drag(move, start, up);
     path.drag(move, start, up);
+
 };
+
+function go(){
+    environment.shapes = Array();
+    paper.forEach(function(e){
+        if(e.attrs.class == "added"){
+            environment.shapes.push({
+                "type" : e.type,
+                "atts" : e.attrs,
+                "matrix" : e.matrix.split()
+            });
+        }
+    });
+    console.clear();
+    console.log(JSON.stringify(environment.shapes));
+    console.log(environment.shapes);
+}
